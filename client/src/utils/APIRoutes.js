e9be9ea91485cb8b041ea8af11e33fcf6a3de1e9
Euro Abao <abaoeuro2002@gmail.com>
2026-02-09 16:20:25 +0800

    clean live

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -3,28 +3,28 @@ export const LOCAL = 'http://localhost:5000'
 export const LIVE = 'https://ebun-monitoring.onrender.com'
 
 // truck
-export const API_TRUCK = `${LOCAL}/api/truck`
+export const API_TRUCK = `${LIVE}/api/truck`
 
 // driver
-export const API_DRIVER = `${LOCAL}/api/driver`
+export const API_DRIVER = `${LIVE}/api/driver`
 
 // admin
-export const API_USER = `${LOCAL}/api/user`
+export const API_USER = `${LIVE}/api/user`
 
 // tool
-export const API_TOOL = `${LOCAL}/api/tool`
+export const API_TOOL = `${LIVE}/api/tool`
 
 // deployment
-export const API_DEPLOYMENT = `${LOCAL}/api/deployment`
+export const API_DEPLOYMENT = `${LIVE}/api/deployment`
 
 // analytics
-export const API_ANALYTICS = `${LOCAL}/api/analytics`
+export const API_ANALYTICS = `${LIVE}/api/analytics`
 
 //  access
-export const API_CLIENT = `${LOCAL}/api/client`
+export const API_CLIENT = `${LIVE}/api/client`
 
 // activitylogs
-export const API_ACTIVITY_LOGS = `${LOCAL}/api/activity-logs`
+export const API_ACTIVITY_LOGS = `${LIVE}/api/activity-logs`
 
 // timeline logs
-export const API_TIMELINE_LOGS = `${LOCAL}/api/timeline-logs`
+export const API_TIMELINE_LOGS = `${LIVE}/api/timeline-logs`
