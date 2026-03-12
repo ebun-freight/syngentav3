7326124006a5be025d2a437dd206f5bed35639ef
Euro Abao <abaoeuro2002@gmail.com>
2026-03-12 10:09:22 +0800

    email port change from 465 to 587

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -372,8 +372,9 @@ const sendStatusEmail = async ({ user, status }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: parseInt(process.env.EMAIL_PORT),
-      secure: true,
+      port: 587,
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
