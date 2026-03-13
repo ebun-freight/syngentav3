175b8252928f14a2235eb6c953229cf87218b571
Euro Abao <abaoeuro2002@gmail.com>
2026-03-14 03:47:11 +0800

    new url to ebun.vercel.app

--- server/index.js
+++ server/index.js
@@ -18,10 +18,7 @@ connectDB()
 const app = express()
 app.use(
   cors({
-    origin: [
-      'https://ebun-monitoring-inky.vercel.app',
-      'http://localhost:5173'
-    ],
+    origin: ['https://ebun.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
