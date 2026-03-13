8cb71d74c522d5eabecbc07e1a963c94a39bf71e
Euro Abao <abaoeuro2002@gmail.com>
2026-03-14 03:21:50 +0800

    new frontend url

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(
   cors({
     origin: [
-      'https://ebun-monitoring-three.vercel.app',
+      'https://ebun-monitoring-eosin.vercel.app',
       'http://localhost:5173'
     ],
     credentials: true,
