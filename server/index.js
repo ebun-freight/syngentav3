ec61fd520e32ff4d85315ad6e86e24191d80ca9e
Euro Abao <abaoeuro2002@gmail.com>
2026-03-14 03:40:01 +0800

    index inky added

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(
   cors({
     origin: [
-      'https://ebun-monitoring-eosin.vercel.app',
+      'https://ebun-monitoring-inky.vercel.app',
       'http://localhost:5173'
     ],
     credentials: true,
