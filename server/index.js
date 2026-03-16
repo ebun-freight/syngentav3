9d5d94ba15855786d812a24fabea0e12933e27d4
Ebun Freight <ebun.freight.opc@gmail.com>
2026-03-16 17:13:53 +0800

    orig url

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ connectDB()
 const app = express()
 app.use(
   cors({
-    origin: ['https://ebun.vercel.app', 'http://localhost:5173'],
+    origin: ['https://smc.ebun.ph', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
