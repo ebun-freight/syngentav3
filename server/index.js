7eca8096026d0dd92816cdaeaa3d886da0669839
Ebun Freight <ebun.freight.opc@gmail.com>
2026-03-16 17:13:16 +0800

    tanga

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ connectDB()
 const app = express()
 app.use(
   cors({
-    origin: ['https://smc.ebun.ph', 'http://localhost:5173'],
+    origin: ['https://ebun.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -26,6 +26,7 @@ app.use(
 )
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 // routes
 app.use('/api/user', require('./routes/userRoute'))
 app.use('/api/driver', require('./routes/driverRoute'))
