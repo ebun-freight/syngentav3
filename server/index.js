b1eceddbc0e67391c9e329698670842a8e3d7c9a
ebun-freight <ebun.freight.opc@gmail.com>
2026-04-01 11:53:02 +0800

    Update index.js to syngenta

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const app = express()
 
 const corsOptions = {
   origin: [
-    'https://syngentav3-apg0u1g2b-ebun-freights-projects.vercel.app',
+    'https://syngenta.ebun.ph',
     'http://localhost:5173'
   ],
   credentials: true,
